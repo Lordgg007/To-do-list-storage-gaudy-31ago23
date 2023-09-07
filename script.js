cd8a7aca47d32e13cabf969869183e7ef2098bad
Lordgg007 <supergaudy1@gmail.com>
2023-09-07 12:29:46 -0400

    fix: add button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Initial References
 const newTaskInput = document.querySelector("#new-task input");
-const taskDiv = document.querySelector("#tasks");
-let deleteTasks = editTasks, tasks;
+const tasksDiv = document.querySelector("#tasks");
+let deleteTasks, editTasks, tasks;
 let updateNote = "";
 let count;
 
@@ -10,18 +10,18 @@ window.onload = () => {
   updateNote = "";
   count = Object.keys(localStorage).length;
   displayTasks();
-}
+};
 
 //Function to Display The Tasks
 const displayTasks = () => {
   if (Object.keys(localStorage).length > 0) {
-    taskDiv.style.display = "inline-block";
+    tasksDiv.style.display = "inline-block";
   } else {
-    taskDiv.style.display = "none";
+    tasksDiv.style.display = "none";
   }
 
   //Clear the tasks
-  taskDiv.innerHTML = "";
+  tasksDiv.innerHTML = "";
 
   //Fetch All The  Keys in local storage
   let tasks = Object.keys(localStorage);
@@ -41,15 +41,15 @@ const displayTasks = () => {
     let editButton = document.createElement("button");
     editButton.classList.add("edit");
     editButton.innerHTML = `<ion-icon name="create"></ion-icon>`;
-    if(!JSON.parse(value)) {
+    if (!JSON.parse(value)) {
       editButton.style.visibility = "visible";
     } else {
       editButton.style.visibility = "hidden";
       taskInnerDiv.classList.add("completed");
     }
     taskInnerDiv.appendChild(editButton);
-    taskInnerDiv.innerHTML += '<button class="delete"><ion-icon name="trash"></ion-icon></button>';
-    taskDiv.appendChild(taskInnerDiv);
+    taskInnerDiv.innerHTML += `<button class="delete"><ion-icon name="trash"></ion-icon></button>`;
+    tasksDiv.appendChild(taskInnerDiv);
   }
 
   //tasks completed
@@ -67,4 +67,75 @@ const displayTasks = () => {
 
   //Edit Tasks
   editTasks = document.getElementsByClassName("edit");
-}
+  Array.from(editTasks).forEach((element, index) => {
+    element.addEventListener("click", (e) => {
+      //Stop propagation to outer elements (if remove when we click dlete eventualy rhw click will move to parent)
+      e.stopPropagation();
+      //disable other edit buttons when one task is being edited
+      disableButtons(true);
+      //updateinput value and remove div
+      let parent = element.parentElement;
+      newTaskInput.value = parent.querySelector("#taskname").innerText; 
+      //set updateNote to the task that is being edited 
+      updateNote = parent.id;
+      //remove task
+      parent.remove();
+    });
+  });
+
+  //Delete task 
+  deleteTasks = document.getElementsByClassName("delete");
+  Array.from(deleteTasks).forEach((element, index) => {
+    element.addEventListener("click", (e) => {
+      e.stopPropagation();
+      //Delte from local storage
+      let parent = element.parentElement;
+      removeTask(parent.id);
+      parent.remove();
+      count -= 1;
+    });
+  });
+};
+
+//Disable edit button 
+const disableButtons = (bool) => {
+  let editButtons = document.getElementsByClassName("edit");
+  Array.from(editButtons).forEach((element) => {
+    element.disabled = bool; 
+  });
+};
+
+//Remove Task from local storage
+const removeTask = (taskValue) => {
+  localStorage.removeItem(taskValue);
+  displayTasks();
+};
+
+//Add task to local storage
+const updateStorage = (index, taskValue, completed) => {
+  localStorage.setItem(`${index}_${taskValue}`, completed);
+  displayTasks();
+};
+
+//FUnction to add new task
+document.querySelector("#push").addEventListener("click", () => {
+  //Enable the edit button 
+  disableButtons(false);
+  if (newTaskInput.value.length == 0) {
+    alert("Please Enter A Task"); 
+  } else {
+    //Store Locally and display from local storage
+    if (updateNote == "") {
+      //new task
+      updateStorage(count, newTaskInput.value, false);
+    } else {
+      //update task
+      let existingCount = updateNote.split("_")[0];
+      removeTask(updateNote);
+      updateStorage(existingCount, newTaskInput.value, false);
+      updateNote = "";
+    }
+    count += 1;
+    newTaskInput.value = "";
+  }
+});
